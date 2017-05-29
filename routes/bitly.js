0c92e925e4c11380137708aab6d6fe84e414e14f
tayutaedomo <tayutaedomo@gmail.com>
2017-05-29 22:23:02 +0900

    Add shorten API

--- routes/bitly.js
+++ routes/bitly.js
@@ -1,9 +1,51 @@
+'use strict';
+
+var debug = require('debug')('node-js-bitly-trial:routes:bitly');
+
 var express = require('express');
 var router = express.Router();
 
+var beautify = require('js-beautify').js_beautify;
+var Bitly = require('bitly');
+
+
+function create_bitly() {
+  return new Bitly(process.env.BITLY_ACCESS_TOKEN)
+}
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/shorten', function(req, res, next) {
+  res.render('bitly/shorten', {
+    title: 'Shorten API',
+    data: {}
+  });
 });
 
+router.post('/shorten', function(req, res, next) {
+  var bitly = create_bitly();
+
+  bitly.shorten(req.body.url).then(function(result) {
+    debug('shorten', result);
+
+    res.render('bitly/shorten', {
+      title: 'Shorten API',
+      data: {
+        result: result,
+        resultStr: beautify(JSON.stringify(result), { indent_size: 2 })
+      }
+    });
+
+  }).catch(function(reason) {
+    res.render('bitly/shorten', {
+      title: 'Shorten API',
+      data: {
+        error: reason,
+        errorStr: beautify(JSON.stringify(reason), { indent_size: 2 })
+      }
+    });
+  });
+});
+
+
 module.exports = router;
+
